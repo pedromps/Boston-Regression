39db20e5a426fd9fe3adfb0282f4a2c725a1aad0
pedromps <90725927+pedromps@users.noreply.github.com>
2021-09-18 12:49:22 +0100

    Update boston.py
    sorted the libraries and added the code to plot
    
    some changes to the NN
    
    calculating the MAE and not the MSE (easily interchangeable)

--- boston.py
+++ boston.py
@@ -1,26 +1,26 @@
 # -*- coding: utf-8 -*-
-from sklearn.datasets import load_boston
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
 # from sklearn.linear_model import LinearRegression
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 # from tensorflow.keras.callbacks import ModelCheckpoint
-from sklearn.model_selection import train_test_split
 
 
 boston_dataset = load_boston()
-print(boston_dataset.keys())
-
 boston = pd.DataFrame(boston_dataset.data, columns=boston_dataset.feature_names)
-
+boston_t = pd.DataFrame(boston_dataset.target)
 
 # lin_model = LinearRegression()
 # lin_model.fit(X_train, Y_train)
 
 data = np.array(boston)
+target = np.array(boston_t)
 #the value to predict is in the last column
-x_train, x_test, y_train, y_test = train_test_split(data[:, 0:data.shape[1]-1], data[:, -1], train_size = 0.8)
+x_train, x_test, y_train, y_test = train_test_split(data, target, train_size = 0.8)
 
 #normalise in respect to the training data
 x_max = np.nanmax(x_train, axis=0)
@@ -40,12 +40,18 @@ y_train = (y_train-y_min)/(y_max-y_min)
 # model_checkpoint_callback = ModelCheckpoint(filepath=checkpoint_filepath, save_weights_only=True, monitor='val_loss', mode='min', save_best_only=True)
 
 model = Sequential()
-model.add(Dense(64, input_dim = 12 , activation = 'relu'))
+model.add(Dense(64, input_dim = 13 , activation = 'relu'))
 model.add(Dense(6, activation = 'relu'))
-model.add(Dense(1, activation = 'relu'))
+model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
 model.summary()
-history = model.fit(x_train, y_train, batch_size = 6, epochs=100, validation_split=0.125)
+history = model.fit(x_train, y_train, batch_size = 32, epochs=200, validation_split=0.125)
+
+#plotting
+plt.figure()
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.grid()
 
 #predicting 
 y_pred = model.predict(x_test)
@@ -53,6 +59,7 @@ y_pred = model.predict(x_test)
 #denormalise to get the real world values
 y_pred = y_pred*(y_max-y_min) + y_min
 
-#calculate MSE
-MSE = np.mean((y_test.ravel()-y_pred.ravel())**2)
-print("MSE of the normalised data = ", MSE)
+#calculate MAE or MSE
+#MSE = np.mean((y_test.ravel()-y_pred.ravel())**2)
+MAE = np.mean(np.abs(y_test.ravel()-y_pred.ravel()))
+print("MAE of the normalised data = ", MAE)
