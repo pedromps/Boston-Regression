5c6d65b8fa310d2c9f50fed6337ea2f27e7f4db4
pedromps <90725927+pedromps@users.noreply.github.com>
2021-09-18 15:48:15 +0100

    Update boston.py
    added correlation calculations
    
    added the linear regression model
    
    fixed the issue with the target not having the name (now called Price)
    
    todo:
    choose which metric to evaluate (RMSE, MAE or MSE)
    tidy up code to run both models with the same data to compare results
    eventually implement kfold or some other cross validation technique

--- boston.py
+++ boston.py
@@ -4,9 +4,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
-# from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
+import seaborn as sns 
 # from tensorflow.keras.callbacks import ModelCheckpoint
 
 
@@ -14,13 +15,36 @@ boston_dataset = load_boston()
 boston = pd.DataFrame(boston_dataset.data, columns=boston_dataset.feature_names)
 boston_t = pd.DataFrame(boston_dataset.target)
 
-# lin_model = LinearRegression()
-# lin_model.fit(X_train, Y_train)
+#to give a name to the target
+boston_t.rename(columns={0: 'Price'}, inplace=True)
 
+
+
+
+
+#to choose only the features with high correlation, the first step is to see how correlated they are
+full_data = pd.concat((boston, boston_t), axis=1)
+correlation_matrix = full_data.corr().round(2)
+# annot = True to print the values inside the square
+sns.heatmap(data=correlation_matrix, annot=True)
+
+#high correlation -> >=0.5. correlation with self = 1 so it's excluded posteriously
+aux = correlation_matrix.loc[np.abs(correlation_matrix.loc['Price'])>=0.5, 'Price']
+aux.drop(index='Price', inplace=True)
+
+#data with only the highly correlated dat
+corr_data = boston[aux.index]
+
+
+#full data
 data = np.array(boston)
 target = np.array(boston_t)
+
+
+# data = corr_data
 #the value to predict is in the last column
 x_train, x_test, y_train, y_test = train_test_split(data, target, train_size = 0.8)
+
 
 #normalise in respect to the training data
 x_max = np.nanmax(x_train, axis=0)
@@ -40,7 +64,7 @@ y_train = (y_train-y_min)/(y_max-y_min)
 # model_checkpoint_callback = ModelCheckpoint(filepath=checkpoint_filepath, save_weights_only=True, monitor='val_loss', mode='min', save_best_only=True)
 
 model = Sequential()
-model.add(Dense(64, input_dim = 13 , activation = 'relu'))
+model.add(Dense(64, input_dim = x_train.shape[1] , activation = 'relu'))
 model.add(Dense(6, activation = 'relu'))
 model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
@@ -63,3 +87,13 @@ y_pred = y_pred*(y_max-y_min) + y_min
 #MSE = np.mean((y_test.ravel()-y_pred.ravel())**2)
 MAE = np.mean(np.abs(y_test.ravel()-y_pred.ravel()))
 print("MAE of the normalised data = ", MAE)
+
+
+
+lin_model = LinearRegression()
+lin_model.fit(x_train, y_train)
+
+y_predi = lin_model.predict(x_test)
+y_predi = y_predi*(y_max-y_min) + y_min
+RMSE = np.mean(np.sqrt((y_test.ravel()-y_predi.ravel())**2))
+print("RMSE of the normalised data = ", RMSE)
